125f081c69fef750bd2782fb739ffb70c5a280bf
Jay <s199064@students.aie.edu>
2020-09-22 18:21:49 -0700

    Workin on UI and Animation Exercises!

---
+++ Assets/Scripts/BasicCharacterMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicCharacterMovement : MonoBehaviour
+{
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+
+        gameObject.transform.Translate(input * Time.deltaTime);
+
+        anim.SetFloat("LocomotionBlend", input.magnitude);
+    }
+}
---
+++ Assets/Scripts/ToggleAnim.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleAnim : MonoBehaviour
+{
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            anim.SetInteger("State", 0);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            anim.SetInteger("State", 1);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            anim.SetInteger("State", 2);
+        }
+    }
+}
